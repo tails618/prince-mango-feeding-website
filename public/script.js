736b4353f826ec3d13c1b072c5d8c93e48f46714
tails618 <asher.grossman@gmail.com>
2021-09-09 21:28:43 -0500

    Set up firebase

---
+++ public/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBAPTjwH5PjyRd7FPNi26kSXkzd5GALnw",
+  authDomain: "prince-mango-feeding-website.firebaseapp.com",
+  projectId: "prince-mango-feeding-website",
+  storageBucket: "prince-mango-feeding-website.appspot.com",
+  messagingSenderId: "311736192147",
+  appId: "1:311736192147:web:df39c2d69110182164d166"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
